ecf3bacdda4064a08e5e77edc572584c8e6226b3
Chrysa Nguyen <vdoan925@students.fairmontschools.com>
2023-11-17 20:25:18 -0800

    Add comments for drive control region

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/HuskyTeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/HuskyTeleOpMode.java
@@ -41,16 +41,27 @@ public class HuskyTeleOpMode extends LinearOpMode {
             // endregion
 
             // region DRIVE CONTROL
+
+            // Press START to reset robot heading.
             if (currentGamepad1.start) {
                 huskyBot.setCurrentHeadingAsForward();
             }
 
+            // Hold LEFT BUMPER to align with an AprilTag.
             if (currentGamepad1.left_bumper &&
                     huskyBot.huskyVision.AprilTagDetector.getAprilTagById(583).isPresent()) {
                 PoseVelocity2d pw = huskyBot.alignWithAprilTag(583);
                 TelemetryUtils.PoseVelocity2d(pw);
                 huskyBot.driveRobot(pw.component1().y, pw.component1().x, pw.component2(), 1.0);
-            } else {
+            }
+            /*
+            * If not LEFT BUMPER, use:
+            *  - A to toggle between field-centric and robot-centric drive.
+            *  - LEFT STICK for movement.
+            *  - RIGHT STICK for rotation.
+            *  - LEFT TRIGGER to increase speed.
+            */
+            else {
                 if (usingFieldCentric.get()) {
                     telemetry.addLine("Currently using field centric");
                     huskyBot.fieldCentricDriveRobot(
