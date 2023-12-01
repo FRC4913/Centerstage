f8df9d9e93eff3528175d948e35ef07df654d6a1
chrysaliszt <vdoan925@students.fairmontschools.com>
2023-11-30 23:37:01 -0800

    Remove old HuskyProp tensorflow models

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/vision/TensorflowDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/vision/TensorflowDetection.java
@@ -8,12 +8,11 @@ public class TensorflowDetection {
 
     // TFOD_MODEL_ASSET points to a model file stored in the project Asset location,
     // this is only used for Android Studio when using models in Assets.
-    private static final String TFOD_MODEL_ASSET = "model_20231019_154144.tflite";
-    private static final String TFOD_Blue_MODEL_ASSET = "Blue.tflite";
-    private static final String TFOD_Red_MODEL_ASSET = "Red.tflite";
+    private static final String TFOD_Blue_MODEL_ASSET = "HuskyPropBlue.tflite";
+    private static final String TFOD_Red_MODEL_ASSET = "HuskyPropRed.tflite";
     // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
     // this is used when uploading models directly to the RC using the model upload interface.
-//    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/model_20231019_154144.tflite";
+
     // Define the labels recognized in the model for TFOD (must be in training order!)
     private static final String[] LABELS = {
             "HuskyProp",
@@ -22,7 +21,6 @@ public class TensorflowDetection {
     /**
      * The variable to store our instance of the TensorFlow Object Detection processor.
      */
-    public TfodProcessor tfod;
     public TfodProcessor tfodBlue;
     public TfodProcessor tfodRed;
 
@@ -38,25 +36,6 @@ public class TensorflowDetection {
 
 
         // Create the TensorFlow processor by using a builder.
-        tfod = new TfodProcessor.Builder().setModelAssetName(TFOD_MODEL_ASSET)
-
-                // With the following lines commented out, the default TfodProcessor Builder
-                // will load the default model for the season. To define a custom model to load,
-                // choose one of the following:
-                //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
-                //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-                //.setModelAssetName(TFOD_MODEL_ASSET)
-                //.setModelFileName(TFOD_MODEL_FILE)
-
-                // The following default settings are available to un-comment and edit as needed to
-                // set parameters for custom models.
-                .setModelLabels(LABELS)
-                //.setIsModelTensorFlow2(true)
-                //.setIsModelQuantized(true)
-                //.setModelInputSize(300)
-                //.setModelAspectRatio(16.0 / 9.0)
-
-                .build();
         tfodBlue = new TfodProcessor.Builder()
                 .setModelAssetName(TFOD_Blue_MODEL_ASSET)
                 .setModelLabels(LABELS)
@@ -85,7 +64,6 @@ public class TensorflowDetection {
 
 
         // Set confidence threshold for TFOD recognitions, at any time.
-        tfod.setMinResultConfidence(0.7f);
         tfodBlue.setMinResultConfidence(0.7f);
         tfodRed.setMinResultConfidence(0.7f);
 
