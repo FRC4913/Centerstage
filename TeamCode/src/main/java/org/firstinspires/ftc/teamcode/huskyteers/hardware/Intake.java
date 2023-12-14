8ac1729bc99213a9fd23d3a4a864d61ee7c2bb45
dancingsquirrel06 <118295532+dancingsquirrel06@users.noreply.github.com>
2023-12-14 15:55:54 -0800

    started intake code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/hardware/Intake.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.huskyteers.hardware;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Config
+public class Intake {
+
+    private final DcMotorEx intakeMotor;
+
+    public Intake(HardwareMap hardwareMap) {
+        intakeMotor = hardwareMap.get(DcMotorEx.class, "intake_motor");
+        intakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        // Set other motor properties if needed
+    }
+
+    public void runIntake(double power) {
+        intakeMotor.setPower(power);
+    }
+
+    public void stopIntake() {
+        intakeMotor.setPower(0);
+    }
+
+    public void reverseIntake(double power) {
+        intakeMotor.setPower(-power);
+    }
+
+    // Add other methods as needed
+}
