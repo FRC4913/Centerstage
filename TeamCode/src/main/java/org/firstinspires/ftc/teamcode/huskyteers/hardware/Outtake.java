1441b710bcf38cc9b3b1b3e05c98ed54c46f2c9d
dancingsquirrel06 <118295532+dancingsquirrel06@users.noreply.github.com>
2024-01-12 16:48:56 -0800

    on progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/hardware/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/hardware/Outtake.java
@@ -3,7 +3,6 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.CRServo;
 @Config
 public class Outtake {
@@ -15,4 +14,19 @@ public class Outtake {
         outtakeServo = hardwareMap.get(CRServo.class, "outtake_servo");
         // Set other motor properties if needed@
     }
+    public void OuttakeUp(double power) {
+        outtakeMotor.setPower(power);
+    }
+
+    public void stopOuttake() {
+        outtakeMotor.setPower(0);
+    }
+
+    public void OuttakeDown(double power) {
+        outtakeMotor.setPower(-power);
+    }
+    public void dump(double power){
+        outtakeServo.setPower(power);
+    }
+    public void set
 }
