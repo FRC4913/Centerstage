ca0fb422d920d7486834dbaf2a1dafce25c1b77c
pairnon <janpairnon@gmail.com>
2024-01-18 17:23:30 -0800

    Invert init heading

--- RobotPaths/src/main/java/com/example/huskyteers/FieldInfo.java
+++ RobotPaths/src/main/java/com/example/huskyteers/FieldInfo.java
@@ -12,13 +12,13 @@ public class FieldInfo {
     public static Pose2d getStartPose(Position position, double thickness) {
         switch (position) {
             case RED_LEFT:
-                return new Pose2d(tiles(-1.5), tiles(-3) + thickness / 2, Math.toRadians(-90));
+                return new Pose2d(tiles(-1.5), tiles(-3) + thickness / 2, Math.toRadians(90));
             case RED_RIGHT:
-                return new Pose2d(tiles(0.5), -tiles(3) + thickness / 2, Math.toRadians(-90));
+                return new Pose2d(tiles(0.5), -tiles(3) + thickness / 2, Math.toRadians(90));
             case BLUE_LEFT:
-                return new Pose2d(tiles(0.5), tiles(3) - thickness / 2, Math.toRadians(90));
+                return new Pose2d(tiles(0.5), tiles(3) - thickness / 2, Math.toRadians(-90));
             case BLUE_RIGHT:
-                return new Pose2d(tiles(-1.5), tiles(3) - thickness / 2, Math.toRadians(90));
+                return new Pose2d(tiles(-1.5), tiles(3) - thickness / 2, Math.toRadians(-90));
             default:
                 return new Pose2d(0, 0, 0);
         }
