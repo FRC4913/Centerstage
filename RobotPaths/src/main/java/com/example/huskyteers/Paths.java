f831c3fadc07a2005dcc10b2f2c675dda058206c
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2024-01-18 16:17:25 -0800

    Remove everything and add stubs

--- RobotPaths/src/main/java/com/example/huskyteers/Paths.java
+++ RobotPaths/src/main/java/com/example/huskyteers/Paths.java
@@ -13,6 +13,7 @@ public class Paths {
      * @return Action to navigate to the team prop
      */
     public static Action pathToTeamProp(TrajectoryActionBuilder actionBuilder, TeamPropLocation teamPropLocation) {
+        // Assigned to Chrysa
         switch (teamPropLocation) {
             case LEFT:
                 return actionBuilder
@@ -41,36 +42,18 @@ public class Paths {
         return actionBuilder.build();
     }
 
-    public static Action reversePathToTeamProp(TrajectoryActionBuilder actionBuilder, TeamPropLocation teamPropLocation) {
-        switch (teamPropLocation) {
-            case LEFT:
-            case RIGHT:
-                return actionBuilder.turnTo(Math.toRadians(0))
-                        .strafeTo(new Vector2d(0, 0)).build();
-            case CENTER:
-                return actionBuilder.strafeTo(new Vector2d(0, 0))
-                        .build();
-        }
+    public static Action pathToParkingFromFrontstage(TrajectoryActionBuilder actionBuilder) {
+        // Assigned to francis
         return actionBuilder.build();
     }
 
-
-    public static Vector2d commonPoint = new Vector2d(-34, -8);
-
-    public static Action pathToCommonPoint(TrajectoryActionBuilder actionBuilder, TeamPropLocation teamPropLocation) {
-        switch (teamPropLocation) {
-            case LEFT:
-            case RIGHT:
-                // Start at 28, -3
-                return actionBuilder.strafeToSplineHeading(commonPoint, 0).build();
-            case CENTER:
-                // Start at 30, 0
-                return actionBuilder.strafeTo(new Vector2d(FieldInfo.tiles(-1.5), FieldInfo.tiles(-1.75))).turnTo(Math.toRadians(180)).splineTo(new Vector2d(-58, -24), Math.toRadians(90)).splineTo(commonPoint, 0).build();
-        }
+    public static Action pathToBackdrop(TrajectoryActionBuilder actionBuilder, TeamPropLocation teamPropLocation) {
+        // Assigned to Ethan
         return actionBuilder.build();
     }
 
-    public static Action pathToBackstageFar(TrajectoryActionBuilder actionBuilder) {
-        return actionBuilder.strafeTo(new Vector2d(58.5, -12.2)).build();
+    public static Action pathToParkingFromBackstage(TrajectoryActionBuilder actionBuilder) {
+        // Assigned to Ethan
+        return actionBuilder.build();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/opmodes/HuskyAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/opmodes/HuskyAuto.java
@@ -2,8 +2,7 @@ package org.firstinspires.ftc.teamcode.huskyteers.opmodes;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SleepAction;
-import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.example.huskyteers.FieldInfo;
 import com.example.huskyteers.Paths;
@@ -25,47 +24,6 @@ public class HuskyAuto extends LinearOpMode {
         position = p;
     }
 
-    public void navigateToTeamPropLocation(TeamPropLocation location) {
-        telemetry.addData("Going to location:", location);
-
-        Actions.runBlocking(Paths.pathToTeamProp(huskyBot.drive.actionBuilder(new Pose2d(0, 0, 0)), location));
-    }
-
-    public void navigateToInitialLocation(TeamPropLocation location) {
-        telemetry.addData("Going to location:", location);
-        Actions.runBlocking(Paths.reversePathToTeamProp(huskyBot.drive.actionBuilder(huskyBot.getDrivePoseEstimate()), location));
-    }
-
-    public void parkInBackstageCloseBack() {
-        // TODO: Only supports when next to backstage. Decide on a strategy for the other side.
-        if (position.equals(Position.BLUE_LEFT)) {
-            Actions.runBlocking(huskyBot.drive.actionBuilder(new Pose2d(0, 0, 0))
-                    .strafeTo(new Vector2d(0, 24 * 2))
-                    .build());
-        } else {
-            Actions.runBlocking(huskyBot.drive.actionBuilder(new Pose2d(0, 0, 0))
-                    .strafeTo(new Vector2d(0, -24 * 2))
-                    .build());
-        }
-    }
-
-    public void parkInBackstageFar() {
-        if (FieldInfo.isRed(position)) {
-            Actions.runBlocking(Paths.pathToBackstageFar(huskyBot.drive.actionBuilder(huskyBot.getDrivePoseEstimate())));
-        } else {
-            Actions.runBlocking(Paths.pathToBackstageFar(huskyBot.drive.reversedActionBuilder(huskyBot.getDrivePoseEstimate())));
-        }
-
-    }
-
-    public void navigateToCommonPoint(TeamPropLocation teamPropLocation) {
-        if (FieldInfo.isRed(position)) {
-            Actions.runBlocking(Paths.pathToCommonPoint(huskyBot.drive.actionBuilder(huskyBot.getDrivePoseEstimate()), teamPropLocation));
-        } else {
-            Actions.runBlocking(Paths.pathToCommonPoint(huskyBot.drive.reversedActionBuilder(huskyBot.getDrivePoseEstimate()), teamPropLocation));
-        }
-    }
-
 
     public TeamPropLocation getPropLocation() {
         // uses 0, 1, 2
@@ -74,46 +32,6 @@ public class HuskyAuto extends LinearOpMode {
         } else {
             return huskyBot.huskyVision.openCv.bluePropLocation();
         }
-    }
-
-    public void waitSeconds(double seconds) {
-        Actions.runBlocking(new SleepAction(seconds));
-    }
-
-    public float reverse(float num) {
-        if (!FieldInfo.isRed(position)) {
-            return -num;
-        }
-        return num;
-    }
-
-    public void closeToBackdropAuto(TeamPropLocation teamPropLocation) {
-        // Go to center of spikes (backwards)
-        Actions.runBlocking(Paths.pathToTeamProp(huskyBot.drive.actionBuilder(new Pose2d(0, 0, Math.toRadians(180))), teamPropLocation));
-
-        if (teamPropLocation == TeamPropLocation.RIGHT) {
-            // Left Spike: Rotate left
-
-            huskyBot.drive.actionBuilder(huskyBot.getDrivePoseEstimate())
-                    .lineToY(reverse(-30))
-                    .turn(Math.toRadians(-90))
-
-                    .build();
-        } else if (teamPropLocation == TeamPropLocation.CENTER) {
-            // Center:Continue backwards motion
-            huskyBot.drive.actionBuilder(huskyBot.getDrivePoseEstimate())
-                    .lineToY(-12)
-                    .build();
-        } else {
-            huskyBot.drive.actionBuilder(huskyBot.getDrivePoseEstimate())
-                    .lineToY(reverse(-30))
-                    .turn(Math.toRadians(90))
-                    .build();
-        }
-    }
-
-    public void farFromBackdropAuto(TeamPropLocation teamPropLocation) {
-
     }
 
     @Override
@@ -126,35 +44,48 @@ public class HuskyAuto extends LinearOpMode {
         if (isStopRequested()) return;
         sleep(1000);
 
+        // At initial location
         TeamPropLocation teamPropLocation = getPropLocation();
-        System.out.println(teamPropLocation.toString());
-        huskyBot.drive.pose = FieldInfo.getStartPose(position, RobotInfo.HEIGHT);
         navigateToTeamPropLocation(teamPropLocation);
-        if (position == Position.BLUE_LEFT || position == Position.RED_RIGHT) {
-            closeToBackdropAuto(teamPropLocation);
+        huskyBot.drive.pose = FieldInfo.getStartPose(position, RobotInfo.HEIGHT);
+        // At initial location, in actual coordinates
+        if (FieldInfo.isBackstage(position)) {
+            navigateToBackdrop(teamPropLocation);
+            placePixelOnBackdrop();
+            navigateToParkingFromBackstage();
         } else {
-            farFromBackdropAuto(teamPropLocation);
+            navigateToParkingFromFrontstage();
         }
-
-        // Put down purple pixel
-//        navigateToTeamPropLocation(teamPropLocation);
-//        if (position == Position.BLUE_LEFT || position == Position.RED_RIGHT) {
-//            Actions.runBlocking(Paths.pathToTeamProp(huskyBot.drive.actionBuilder(new Pose2d(0, 0, Math.toRadians(180))), teamPropLocation));
-//            navigateToInitialLocation(teamPropLocation);
-//            parkInBackstageCloseBack();
-//        }
-//        if (position == Position.BLUE_RIGHT || position == Position.RED_LEFT) {
-//            Pose2d startPose = FieldInfo.getStartPose(position, 18);
-//            Pose2d updatedPose = huskyBot.drive.pose;
-//
-//            if (position == Position.RED_LEFT) {
-//                huskyBot.drive.pose = new Pose2d(startPose.position.x + updatedPose.position.y, startPose.position.y + updatedPose.position.x, startPose.heading.toDouble() + updatedPose.heading.toDouble());
-//            } else {
-//                huskyBot.drive.pose = new Pose2d(startPose.position.x + updatedPose.position.y, startPose.position.y - updatedPose.position.x, startPose.heading.toDouble() - updatedPose.heading.toDouble());
-//            }
-//            navigateToCommonPoint(teamPropLocation);
-//            waitSeconds(13);
-//            parkInBackstageFar();
-//        }
     }
+
+    public TrajectoryActionBuilder getActionBuilder() {
+        if (!FieldInfo.isRed(position)) {
+            return huskyBot.drive.actionBuilder(huskyBot.getDrivePoseEstimate());
+        }
+        return huskyBot.drive.reversedActionBuilder(huskyBot.getDrivePoseEstimate());
+    }
+
+    public void navigateToTeamPropLocation(TeamPropLocation location) {
+        // Assigned to chrysa
+        Actions.runBlocking(Paths.pathToTeamProp(huskyBot.drive.actionBuilder(new Pose2d(0, 0, 0)), location));
+    }
+
+    private void navigateToBackdrop(TeamPropLocation teamPropLocation) {
+        // Assigned to Ethan
+        Actions.runBlocking(Paths.pathToBackdrop(getActionBuilder(), teamPropLocation));
+    }
+
+    private void placePixelOnBackdrop() {
+        // Motor thingy, unassigned
+    }
+
+    private void navigateToParkingFromBackstage() {
+        Actions.runBlocking(Paths.pathToParkingFromBackstage(getActionBuilder()));
+    }
+
+    private void navigateToParkingFromFrontstage() {
+        Actions.runBlocking(Paths.pathToParkingFromFrontstage(getActionBuilder()));
+    }
+
+
 }
