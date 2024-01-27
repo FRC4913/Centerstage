60daf2f8f2d69c7c7903746e36e95b0b0ad3e6de
dancingsquirrel06 <118295532+dancingsquirrel06@users.noreply.github.com>
2024-01-26 20:44:17 -0800

    started dronelauncher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/hardware/DroneLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/hardware/DroneLauncher.java
@@ -2,28 +2,22 @@ package org.firstinspires.ftc.teamcode.huskyteers.hardware;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
-import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @Config
 public class DroneLauncher {
-    public static double LAUNCHER_POWER = 0.5;
-    public static double LAUNCHER_DISTANCE = 10;
+    public static double LAUNCHER_LOADED_POSITION;
+    public static double LAUNCHER_LAUNCH_POSITION;
 
-    public final CRServo servo;
+    public final Servo drone_servo;
 
     public DroneLauncher(HardwareMap hardwareMap) {
-        servo = hardwareMap.get(CRServo.class, "drone_launcher");
+        drone_servo = hardwareMap.get(Servo.class, "drone_launcher");
     }
 
     public void shootDrone() {
-        try {
-            servo.setPower(0.5);
-            Thread.sleep(1500);
-            servo.setPower(0.0);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt(); // Restore the interrupted status
-        }
+            drone_servo.setPosition(LAUNCHER_LAUNCH_POSITION);
     }
 
 }
